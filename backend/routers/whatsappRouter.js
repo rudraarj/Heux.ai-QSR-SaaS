6ef61d0f11d09fb4dc587ef47fd32aaac1245932
Balwant Verma <117533183+iambalwant@users.noreply.github.com>
2025-10-06 03:56:55 +0530

    Delete backend directory

--- backend/routers/whatsappRouter.js
+++
@@ -1,9 +0,0 @@
-// const express = require('express')
-// const whatsappController = require('../constrollers/whatsappController')
-
-// const router = express.Router()
-
-// router.get('/webhook',whatsappController.getWebhook)
-// router.post('/webhook',whatsappController.conversationWebhook)
-
-// module.exports = router
